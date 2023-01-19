6520b604b47dc7cb8fe763286e3c36f70404d87b
Csaba79-coder <csabavadasz79@gmail.com>
2023-01-19 18:46:05 +0100

    creat model for project

---
+++ src/main/java/com/csaba79coder/ichmagjavaundspring/domain/Love.java
@@ -0,0 +1,10 @@
+package com.csaba79coder.ichmagjavaundspring.domain;
+
+import lombok.Getter;
+
+@Getter
+public class Love {
+
+    private final String dreamJobOffer = "Ja, das ist mein Dream Job!";
+    private final String dreamJobOpportunity = "JA, das ist mein Dream Job!";
+}
